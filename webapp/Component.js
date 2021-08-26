c0a987bb5f83b7573a9e3bba1f90a0c4106da46a
BokSunJu <bsj6277@naver.com>
2021-08-26 14:33:45 +0900

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("com.confio3.zdemoalpflight12.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
